0aff6555d6adda0404ea4dc9412eac4629f817a0
emran-islam <emran_islam@hotmail.co.uk>
2024-01-19 09:51:50 +0000

    completed task 7 - CORE: POST /api/articles/:article_id/comments

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const {
+  incorrectEndpointHandler,
+  customErrorHandler,
+  psqlErrorHandler,
+  internalServerError,
+} = require("./controllers/errors.controllers");
+
 const app = express();
 
 const {
@@ -7,12 +14,25 @@ const {
   getArticleByID,
   getArticles,
   getCommentsByArticleID,
+  postCommentsByArticleID,
 } = require("./controllers/nc-news.controllers");
 
+app.use(express.json());
+
+// GET requests
 app.get("/api/topics", getTopics);
 app.get("/api", getDescOnOtherEndpoints);
 app.get("/api/articles/:article_id", getArticleByID);
 app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id/comments", getCommentsByArticleID);
+
+//POST requests
+app.post("/api/articles/:article_id/comments", postCommentsByArticleID);
+
+//Errors
+app.use("*", incorrectEndpointHandler);
+app.use(customErrorHandler);
+app.use(psqlErrorHandler);
+app.use(internalServerError);
 
 module.exports = app;
--- models/nc-news.models.js
+++ models/nc-news.models.js
@@ -1,7 +1,9 @@
 const db = require("../db/connection");
 
 exports.fetchTopics = () => {
-  return db.query("SELECT * FROM topics").then((result) => result.rows);
+  return db.query(`SELECT * FROM topics;`).then((results) => {
+    return results.rows;
+  });
 };
 
 exports.fetchArticleByID = (articleID) => {
@@ -11,30 +13,54 @@ exports.fetchArticleByID = (articleID) => {
 };
 
 exports.fetchArticles = () => {
-  const query = `
-    SELECT 
-      articles.*,
-      COUNT(comments.comment_id) AS comment_count
-    FROM articles
-    LEFT JOIN comments ON articles.article_id = comments.article_id
-    GROUP BY articles.article_id
-    ORDER BY created_at DESC;
-  `;
-
-  return db.query(query).then((result) => {
-    return result.rows;
-  });
+  return db
+    .query(
+      `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id) AS comment_count
+      FROM articles
+      LEFT JOIN comments
+      ON articles.article_id = comments.article_id
+      GROUP BY articles.article_id
+      ORDER BY created_at DESC;`
+    )
+    .then((result) => {
+      if (result.rows.length) {
+        return result.rows;
+      } else {
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      }
+    });
 };
-
-exports.fetchCommentsByArticleID = (articleID) => {
-  const query = `
-    SELECT *
-    FROM comments
+exports.fetchCommentsByArticleID = (article_id) => {
+  return db
+    .query(
+      `
+    SELECT * FROM comments
     WHERE article_id = $1
-    ORDER BY created_at DESC;
-  `;
-
-  return db.query(query, [articleID]).then((result) => {
-    return result.rows;
-  });
+    ORDER BY created_at DESC;`,
+      [article_id]
+    )
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `No comments found`,
+        });
+      }
+      return result.rows;
+    });
+};
+exports.addComment = (article_id, username, body) => {
+  return db
+    .query(
+      `
+    INSERT INTO comments
+  (article_id, author, body)
+  VALUES ($1, $2, $3)
+  RETURNING *;`,
+      [article_id, username, body]
+    )
+    .then((result) => {
+      result;
+      return result.rows[0];
+    });
 };
